3175a5dc74e8ca8092f087414f90ff35e0ef4c69
Derek Collison <derek.collison@gmail.com>
2015-08-07 15:40:44 -0700

    Use update.sh to build proper version

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # nats-docker
-Official Docker image for the NATS server
+
+This is the Git repo of the official Docker image for [nats](https://registry.hub.docker.com/_/nats/). See the
+Hub page for the full readme on how to use this Docker image and for information regarding contributing and issues.
---
+++ update.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+if [ $# -eq 0 ] ; then
+	echo "Usage: ./update.sh <nats-io/gnatsd tag or branch>"
+	exit
+fi
+
+VERSION=$1
+
+# cd to the current directory so the script can be run from anywhere.
+cd `dirname $0`
+
+echo "Fetching and building gnatsd $VERSION..."
+
+# Create a tmp build directory.
+TEMP=/tmp/nats.build
+mkdir $TEMP
+
+git clone -b $VERSION https://github.com/nats-io/gnatsd $TEMP
+docker build -t nats-builder $TEMP
+
+# Create a dummy nats builder container so we can run a cp against it.
+ID=$(docker create nats-builder)
+
+# Update the local binary.
+docker cp $ID:/gopath/bin/gnatsd .
+
+# Cleanup.
+rm -fr $TEMP
+docker rm -f $ID
+docker rmi nats-builder
+
+echo "Done."
