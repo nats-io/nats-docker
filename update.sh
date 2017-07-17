8d75c2aab2f16249328ad473c2baf171ae52506e
Ivan Kozlovic <ivan.kozlovic@apcera.com>
2017-07-17 14:09:33 -0600

    Updated Windows variants
    update.sh will now also use gnatsd/Dockerfile.win64 to build the
    executable and copy it to windows sub directories.
    These directories have now an updated Dockerfile that simply
    copy the local gnatsd.exe into the Docker image (instead of
    downloading from release pages).

--- update.sh
+++ update.sh
@@ -28,9 +28,18 @@ ID=$(docker create nats-builder)
 # Update the local binary.
 docker cp $ID:/go/bin/gnatsd .
 
+# Windows variant
+docker build -t nats-builder-win -f $TEMP/Dockerfile.win64 $TEMP
+ID_WIN=$(docker create nats-builder-win)
+docker cp $ID_WIN:/go/src/github.com/nats-io/gnatsd/gnatsd.exe .
+cp gnatsd.exe windows/nanoserver
+cp gnatsd.exe windows/windowsservercore
+rm gnatsd.exe
+
 # Cleanup.
 rm -fr $TEMP
 docker rm -f $ID
+docker rm -f $ID_WIN
 docker rmi nats-builder
-
+docker rmi nats-builder-win
 echo "Done."
