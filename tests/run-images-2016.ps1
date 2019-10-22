19f351c5461bafefcfd8bf9150d475196f930aa2
Jaime Piña <jaime@synadia.com>
2019-10-22 11:35:45 -0700

    Add windows run tests

---
+++ tests/run-images-2016.ps1
@@ -0,0 +1,21 @@
+# Show statements as they run.
+Set-PSDebug -Trace 2
+# Exit on error.
+$ErrorActionPreference = "Stop"
+
+$images = @(
+	"nats:2.1.0-windowsservercore-ltsc2016"
+)
+
+foreach ($img in $images) {
+	Write-Output "running ${img}"
+	$runId = & docker run --detach "${img}"
+	sleep 1
+
+	Write-Output "checking ${img}"
+	$status = & docker ps --filter "id=${runId}" --filter "status=running" --quiet
+	if (!$status) {
+		exit 1
+	}
+	docker kill $runId
+}
