5c637701b98c2b9011845d36f4b8d8f36eebe803
Jaime Piña <jaime@synadia.com>
2019-10-22 11:10:34 -0700

    Separate server version in tests for easy updating

--- tests/build-images-2019.ps1
+++ tests/build-images-2019.ps1
@@ -3,13 +3,15 @@ Set-PSDebug -Trace 2
 # Exit on error.
 $ErrorActionPreference = "Stop"
 
+$ver = 'NATS_SERVER 2.1.0'.Split(' ')[1]
+
 Write-Output '-- host info ---'
 Write-Output $PSVersionTable
 Write-Output (Get-WMIObject win32_operatingsystem).name
 Write-Output (Get-WMIObject win32_operatingsystem).OSArchitecture
 
 # The windowsservercore images must be built before the nanoserver images.
-cd 2.1.0/windowsservercore-1809
+cd "${ver}/windowsservercore-1809"
 Write-Host "building windowsservercore-1809"
 docker build --tag nats:2.1.0-windowsservercore-1809 .
 cd ../nanoserver-1809
