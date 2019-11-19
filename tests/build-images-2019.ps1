41e56d0d2c0dd66d9880bc7d00acdc1243dff07c
Jaime Piña <jaime@synadia.com>
2019-11-18 17:39:32 -0800

    Update images to 2.1.2

--- tests/build-images-2019.ps1
+++ tests/build-images-2019.ps1
@@ -3,7 +3,7 @@ Set-PSDebug -Trace 2
 # Exit on error.
 $ErrorActionPreference = "Stop"
 
-$ver = 'NATS_SERVER 2.1.0'.Split(' ')[1]
+$ver = 'NATS_SERVER 2.1.2'.Split(' ')[1]
 
 Write-Output '-- host info ---'
 Write-Output $PSVersionTable
@@ -13,10 +13,10 @@ Write-Output (Get-WMIObject win32_operatingsystem).OSArchitecture
 # The windowsservercore images must be built before the nanoserver images.
 cd "${ver}/windowsservercore-1809"
 Write-Host "building windowsservercore-1809"
-docker build --tag nats:2.1.0-windowsservercore-1809 .
+docker build --tag nats:2.1.2-windowsservercore-1809 .
 cd ../nanoserver-1809
 Write-Host "building nanoserver-1809"
-docker build --tag nats:2.1.0-nanoserver-1809 .
+docker build --tag nats:2.1.2-nanoserver-1809 .
 
 # We can't test these images on GitHub CI. We get the following error on both
 # windows-2019 and windows-2016.
@@ -24,9 +24,9 @@ docker build --tag nats:2.1.0-nanoserver-1809 .
 #
 # cd ../windowsservercore-1803
 # Write-Host "building windowsservercore-1803"
-# docker build --tag nats:2.1.0-windowsservercore-1803 .
+# docker build --tag nats:2.1.2-windowsservercore-1803 .
 # cd ../nanoserver-1803
 # Write-Host "building nanoserver-1803"
-# docker build --tag nats:2.1.0-nanoserver-1803 .
+# docker build --tag nats:2.1.2-nanoserver-1803 .
 
 docker images
