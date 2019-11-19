2144c5d62a85cc1e2c3943e9ca4a06ecd80e9d92
Jaime Piña <jaime@synadia.com>
2019-11-18 17:33:22 -0800

    Remove sha256

--- update-server-version.sh
+++ update-server-version.sh
@@ -14,23 +14,18 @@ fi
 
 current_version=$(ls -1 | sort | head -n 1)
 new_version="${1}"
-linux_release_sha256="${2}"
-if [[ "${new_version}" == "" ]] || [[ "${linux_release_sha256}" == "" ]]; then
-	echo "usage: ${0} <server version> <linux release sha256>"
-	echo "       ${0} 2.1.0 68e656b251e67e8358bef8483ab0d51c6619f3e7a1a9f0e75838d41ff368f728"
+if [[ "${new_version}" == "" ]]; then
+	echo "usage: ${0} <server version>"
+	echo "       ${0} 2.1.0"
 	exit 1
 fi
 
 echo "current version: ${current_version}"
 echo "new version: ${new_version}"
-echo "linux release sha256: ${linux_release_sha256}"
 
 echo "updating files..."
 files=$(grep --recursive --binary-files=without-match --files-with-matches --extended-regexp "NATS_SERVER [0-9]+\.[0-9]+\.[0-9]+" ./*)
 sedi "s/NATS_SERVER [0-9]\+\.[0-9]\+\.[0-9]\+/NATS_SERVER ${new_version}/g" $files
-
-files=$(grep --recursive --binary-files=without-match --files-with-matches --extended-regexp "NATS_SERVER_SHA256 [a-z0-9]{64}\b" ./*)
-sedi "s/NATS_SERVER_SHA256 [a-z0-9]\{64\}\>/NATS_SERVER_SHA256 ${linux_release_sha256}/g" $files
 
 files=$(grep --recursive --binary-files=without-match --files-with-matches --extended-regexp "nats:[0-9]+\.[0-9]+\.[0-9]+" ./*)
 sedi "s/nats:[0-9]\+\.[0-9]\+\.[0-9]\+/nats:${new_version}/g" $files
