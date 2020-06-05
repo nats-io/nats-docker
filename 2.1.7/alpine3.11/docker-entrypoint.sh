a24499a62a425656d693954cd507ff3360f7cb32
Ivan Kozlovic <ivan@synadia.com>
2020-06-05 17:25:40 -0600

    [FIXED] Alpine entrypoint (#38)
    [FIXED] Alpine entrypoint
    
    The previous PR was not the proper way to do it.
    Here is some background: https://github.com/docker-library/official-images/pull/8137#issuecomment-639843407
    
    Resolves #36
    
    Signed-off-by: Ivan Kozlovic <ivan@synadia.com>

---
+++ 2.1.7/alpine3.11/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+# this if will check if the first argument is a flag
+# but only works if all arguments require a hyphenated flag
+# -v; -SL; -f arg; etc will work, but not arg1 arg2
+if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
+    set -- nats-server "$@"
+fi
+
+# check for the expected command
+if [ "$1" = 'nats-server' ]; then
+    exec "$@"
+fi
+
+# else default to run whatever the user wanted like "bash" or "sh"
+exec "$@"
+
