5902e3e86a6786ee1c023b63741c6de2fde891a8
Ivan Kozlovic <ivan@synadia.com>
2021-06-23 13:31:46 -0600

    Release v2.3.0
    Signed-off-by: Ivan Kozlovic <ivan@synadia.com>

--- tests/run-images-2016.ps1
+++ tests/run-images-2016.ps1
@@ -4,7 +4,7 @@ Set-PSDebug -Trace 2
 $ErrorActionPreference = "Stop"
 
 $images = @(
-	"nats:2.2.6-windowsservercore-ltsc2016"
+	"nats:2.3.0-windowsservercore-ltsc2016"
 )
 
 foreach ($img in $images) {
