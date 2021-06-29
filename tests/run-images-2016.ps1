b814b411d6ceb27e0aa7e3307f1d9e0401cb3c13
Ivan Kozlovic <ivan@synadia.com>
2021-06-29 14:40:19 -0600

    Release v2.3.1
    Update to alpine 3.14
    
    Signed-off-by: Ivan Kozlovic <ivan@synadia.com>

--- tests/run-images-2016.ps1
+++ tests/run-images-2016.ps1
@@ -4,7 +4,7 @@ Set-PSDebug -Trace 2
 $ErrorActionPreference = "Stop"
 
 $images = @(
-	"nats:2.3.0-windowsservercore-ltsc2016"
+	"nats:2.3.1-windowsservercore-ltsc2016"
 )
 
 foreach ($img in $images) {
