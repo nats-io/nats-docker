65352824964adda5e6fb1456b901180041639ccc
Ivan Kozlovic <ivan@synadia.com>
2021-09-23 18:18:35 -0600

    Release v2.6.1
    Signed-off-by: Ivan Kozlovic <ivan@synadia.com>

--- tests/build-images.sh
+++ tests/build-images.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -ex
 
-ver=(NATS_SERVER 2.6.0)
+ver=(NATS_SERVER 2.6.1)
 
 (
 	cd "${ver[1]}/alpine3.14"
-	docker build --tag nats:2.6.0-alpine3.14 .
+	docker build --tag nats:2.6.1-alpine3.14 .
 )
 
 (
 	cd "${ver[1]}/scratch"
-	docker build --tag nats:2.6.0-scratch .
+	docker build --tag nats:2.6.1-scratch .
 )
