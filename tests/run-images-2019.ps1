fdbd23299421b87f35b5112d1a45fbcf6cb96e8d
Ivan Kozlovic <ivan@synadia.com>
2022-04-18 16:38:48 -0600

    Release v2.8.0
    Signed-off-by: Ivan Kozlovic <ivan@synadia.com>

--- tests/run-images-2019.ps1
+++ tests/run-images-2019.ps1
@@ -4,8 +4,8 @@ Set-PSDebug -Trace 2
 $ErrorActionPreference = "Stop"
 
 $images = @(
-	"nats:2.7.4-windowsservercore-1809",
-	"nats:2.7.4-nanoserver-1809"
+	"nats:2.8.0-windowsservercore-1809",
+	"nats:2.8.0-nanoserver-1809"
 )
 
 foreach ($img in $images) {
