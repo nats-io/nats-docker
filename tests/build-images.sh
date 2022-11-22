637747ea58729308b20d4290d821ba6b7516c30d
Phil Pennock <pdp@synadia.com>
2022-11-22 16:07:39 -0500

    Release v2.9.8
    Signed-off-by: Phil Pennock <pdp@synadia.com>

--- tests/build-images.sh
+++ tests/build-images.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -ex
 
-ver=(NATS_SERVER 2.9.7)
+ver=(NATS_SERVER 2.9.8)
 
 (
 	cd "${ver[1]}/alpine3.16"
-	docker build --tag nats:2.9.7-alpine3.16 .
+	docker build --tag nats:2.9.8-alpine3.16 .
 )
 
 (
 	cd "${ver[1]}/scratch"
-	docker build --tag nats:2.9.7-scratch .
+	docker build --tag nats:2.9.8-scratch .
 )
--- tests/run-images-2019.ps1
+++ tests/run-images-2019.ps1
@@ -4,8 +4,8 @@ Set-PSDebug -Trace 2
 $ErrorActionPreference = "Stop"
 
 $images = @(
-	"nats:2.9.7-windowsservercore-1809",
-	"nats:2.9.7-nanoserver-1809"
+	"nats:2.9.8-windowsservercore-1809",
+	"nats:2.9.8-nanoserver-1809"
 )
 
 foreach ($img in $images) {
