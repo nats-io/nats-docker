a7917b933c57aa43adb4d73f4fd7733a0fb545dd
Byron Ruth <byron@nats.io>
2023-06-13 16:03:29 -0400

    Update NATS server to 2.9.18 (#108)
    Signed-off-by: Byron Ruth <byron@nats.io>

--- tests/build-images.sh
+++ tests/build-images.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -ex
 
-ver=(NATS_SERVER 2.9.17)
+ver=(NATS_SERVER 2.9.18)
 
 (
 	cd "${ver[1]}/alpine3.18"
-	docker build --tag nats:2.9.17-alpine3.18 .
+	docker build --tag nats:2.9.18-alpine3.18 .
 )
 
 (
 	cd "${ver[1]}/scratch"
-	docker build --tag nats:2.9.17-scratch .
+	docker build --tag nats:2.9.18-scratch .
 )
