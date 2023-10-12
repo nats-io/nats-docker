f1495d3e08bae56fdd7b91952149a75bc7cb5c64
Waldemar Quevedo <wally@nats.io>
2023-10-12 16:57:25 -0700

    Build fixes for windows
    Signed-off-by: Waldemar Quevedo <wally@nats.io>

--- tests/build-images-2019.ps1
+++ tests/build-images-2019.ps1
@@ -3,7 +3,7 @@ Set-PSDebug -Trace 2
 # Exit on error.
 $ErrorActionPreference = "Stop"
 
-$ver = 'NATS_SERVER 2.9.x'.Split(' ')[1]
+$ver = 'NATS_SERVER 2.9.23'.Split(' ')[1]
 
 Write-Output '-- host info ---'
 Write-Output $PSVersionTable
@@ -11,7 +11,31 @@ Write-Output (Get-WMIObject win32_operatingsystem).name
 Write-Output (Get-WMIObject win32_operatingsystem).OSArchitecture
 
 # The windowsservercore images must be built before the nanoserver images.
-cd "${ver}/windowsservercore-1809"
+cd "2.9.x/windowsservercore-1809"
+Write-Host "building windowsservercore-1809"
+docker build --tag "nats:${ver}-windowsservercore-1809" .
+if ($LASTEXITCODE -ne 0) {
+    exit 1
+}
+
+cd ../nanoserver-1809
+Write-Host "building nanoserver-1809"
+docker build --tag "nats:${ver}-nanoserver-1809" .
+if ($LASTEXITCODE -ne 0) {
+    exit 1
+}
+
+cd ../..
+
+$ver = 'NATS_SERVER 2.10.3'.Split(' ')[1]
+
+Write-Output '-- host info ---'
+Write-Output $PSVersionTable
+Write-Output (Get-WMIObject win32_operatingsystem).name
+Write-Output (Get-WMIObject win32_operatingsystem).OSArchitecture
+
+# The windowsservercore images must be built before the nanoserver images.
+cd "2.10.x/windowsservercore-1809"
 Write-Host "building windowsservercore-1809"
 docker build --tag "nats:${ver}-windowsservercore-1809" .
 if ($LASTEXITCODE -ne 0) {
