6ba8e35e1781582f05f612266b92a5ff59465639
Byron Ruth <byron@nats.io>
2023-12-06 16:19:11 -0500

    Update NATS 2.10.x -> 2.10.7
    Signed-off-by: Byron Ruth <byron@nats.io>

--- tests/build-images.sh
+++ tests/build-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-ver=(NATS_SERVER 2.10.6)
+ver=(NATS_SERVER 2.10.7)
 
 (
 	cd "2.9.x/alpine3.18"
@@ -13,7 +13,7 @@ ver=(NATS_SERVER 2.10.6)
 	docker build --tag nats:${ver[1]}-scratch .
 )
 
-ver=(NATS_SERVER 2.10.6)
+ver=(NATS_SERVER 2.10.7)
 
 (
 	cd "2.10.x/alpine3.18"
