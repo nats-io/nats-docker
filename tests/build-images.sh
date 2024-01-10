4153a6382f66d9f6d482967856ddc14a8e537305
Byron Ruth <byron@nats.io>
2024-01-10 18:36:18 -0500

    Revert alpine update for 2.9.x
    Signed-off-by: Byron Ruth <byron@nats.io>

--- tests/build-images.sh
+++ tests/build-images.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 set -ex
 
-ver=(NATS_SERVER 2.10.8)
+ver=(NATS_SERVER 2.9.24)
 
 (
-	cd "2.9.x/alpine3.19"
-	docker build --tag nats:${ver[1]}-alpine3.19 .
+	cd "2.9.x/alpine3.18"
+	docker build --tag nats:${ver[1]}-alpine3.18 .
 )
 
 (
