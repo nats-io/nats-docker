e7153cd76185c625bfef5384c81db4c96556173a
Byron Ruth <byron@nats.io>
2024-02-15 17:18:30 -0500

    Update NATS 2.10.x -> 2.10.11 (#141)
    Signed-off-by: Byron Ruth <byron@nats.io>

--- 2.10.x/tests/build-images.sh
+++ 2.10.x/tests/build-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-ver=(NATS_SERVER 2.10.10)
+ver=(NATS_SERVER 2.10.11)
 
 (
 	cd "../alpine3.19"
--- 2.10.x/tests/run-images-2019.ps1
+++ 2.10.x/tests/run-images-2019.ps1
@@ -3,7 +3,7 @@ Set-PSDebug -Trace 2
 # Exit on error.
 $ErrorActionPreference = "Stop"
 
-$ver = "NATS_SERVER 2.10.10".Split(" ")[1]
+$ver = "NATS_SERVER 2.10.11".Split(" ")[1]
 
 $images = @(
 	"nats:${ver}-windowsservercore-1809",
