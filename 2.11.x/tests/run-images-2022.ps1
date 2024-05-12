e7d09c35c936e1b4457416466e22e5e856b6a0fd
Byron Ruth <byron@nats.io>
2024-05-11 21:24:46 -0400

    Add tests
    Signed-off-by: Byron Ruth <byron@nats.io>

---
+++ 2.11.x/tests/run-images-2022.ps1
@@ -0,0 +1,24 @@
+# Show statements as they run.
+Set-PSDebug -Trace 2
+# Exit on error.
+$ErrorActionPreference = "Stop"
+
+$ver = "NATS_SERVER 2.11.0-preview.2".Split(" ")[1]
+
+$images = @(
+	"nats:${ver}-windowsservercore-ltsc2022",
+	"nats:${ver}-nanoserver-ltsc2022"
+)
+
+foreach ($img in $images) {
+	Write-Output "running ${img}"
+	$runId = & docker run --detach "${img}"
+	sleep 1
+
+	Write-Output "checking ${img}"
+	docker ps --filter "id=${runId}" --filter "status=running" --quiet
+	if ($LASTEXITCODE -ne 0) {
+		exit 1
+	}
+	docker kill $runId
+}
