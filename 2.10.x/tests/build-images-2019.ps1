6a708a035df929d5be180c23c9b6de15dbe981b8
Neil Twigg <neil@nats.io>
2024-12-17 17:16:29 +0000

    Release v2.10.24
    Signed-off-by: Neil Twigg <neil@nats.io>

--- 2.10.x/tests/build-images-2019.ps1
+++ 2.10.x/tests/build-images-2019.ps1
@@ -3,7 +3,7 @@ Set-PSDebug -Trace 2
 # Exit on error.
 $ErrorActionPreference = "Stop"
 
-$ver = 'NATS_SERVER 2.10.24-RC.3'.Split(' ')[1]
+$ver = 'NATS_SERVER 2.10.24'.Split(' ')[1]
 
 Write-Output '-- host info ---'
 Write-Output $PSVersionTable
