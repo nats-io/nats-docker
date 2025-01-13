f7e6e004d5fbd617209fdbcd175db7465ecac630
Neil <git@neilalexander.dev>
2025-01-13 19:38:42 +0000

    Release v2.10.25-RC.2 (#211)
    Signed-off-by: Neil Twigg <neil@nats.io>

--- 2.10.x/tests/build-images.sh
+++ 2.10.x/tests/build-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-ver=(NATS_SERVER 2.10.24)
+ver=(NATS_SERVER 2.10.25-RC.2)
 
 (
 	cd "../alpine3.21"
--- 2.10.x/tests/run-images-2019.ps1
+++ 2.10.x/tests/run-images-2019.ps1
@@ -3,7 +3,7 @@ Set-PSDebug -Trace 2
 # Exit on error.
 $ErrorActionPreference = "Stop"
 
-$ver = "NATS_SERVER 2.10.24".Split(" ")[1]
+$ver = "NATS_SERVER 2.10.25-RC.2".Split(" ")[1]
 
 $images = @(
 	"nats:${ver}-windowsservercore-1809",
