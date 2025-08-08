9f9520394ef2b5c95b26b0f3c8414d01c2194459
Neil Twigg <neil@nats.io>
2025-08-08 17:24:29 +0100

    Release v2.12.0-preview.1
    Signed-off-by: Neil Twigg <neil@nats.io>

---
+++ 2.12.x/tests/build-images-ltsc2025.ps1
@@ -0,0 +1,28 @@
+# Show statements as they run.
+Set-PSDebug -Trace 2
+# Exit on error.
+$ErrorActionPreference = "Stop"
+
+$ver = 'NATS_SERVER 2.12.0-preview.1'.Split(' ')[1]
+
+Write-Output '-- host info ---'
+Write-Output $PSVersionTable
+Write-Output (Get-WMIObject win32_operatingsystem).name
+Write-Output (Get-WMIObject win32_operatingsystem).OSArchitecture
+
+# The windowsservercore images must be built before the nanoserver images.
+cd "../windowsservercore-ltsc2025"
+Write-Host "building windowsservercore-ltsc2025"
+docker build --tag "nats:${ver}-windowsservercore-ltsc2025" .
+if ($LASTEXITCODE -ne 0) {
+    exit 1
+}
+
+cd "../nanoserver-ltsc2025"
+Write-Host "building nanoserver-ltsc2025"
+docker build --tag "nats:${ver}-nanoserver-ltsc2025" .
+if ($LASTEXITCODE -ne 0) {
+    exit 1
+}
+
+docker images
---
+++ 2.12.x/tests/build-images.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -ex
+
+ver=(NATS_SERVER 2.12.0-preview.1)
+
+(
+	cd "../alpine3.22"
+	docker build --tag nats:${ver[1]}-alpine3.22 .
+)
+
+(
+	cd "../scratch"
+	docker build --tag nats:${ver[1]}-scratch .
+)
