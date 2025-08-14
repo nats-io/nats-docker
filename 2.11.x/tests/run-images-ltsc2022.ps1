10b4d9992e89cf6259a1b7fdb0759df18eb703d3
Neil Twigg <neil@nats.io>
2025-08-14 16:18:51 +0100

    Release v2.11.8
    Signed-off-by: Neil Twigg <neil@nats.io>

--- 2.11.x/tests/run-images-ltsc2022.ps1
+++ 2.11.x/tests/run-images-ltsc2022.ps1
@@ -3,7 +3,7 @@ Set-PSDebug -Trace 2
 # Exit on error.
 $ErrorActionPreference = "Stop"
 
-$ver = "NATS_SERVER 2.11.8-RC.1".Split(" ")[1]
+$ver = "NATS_SERVER 2.11.8".Split(" ")[1]
 
 $images = @(
 	"nats:${ver}-windowsservercore-ltsc2022",
