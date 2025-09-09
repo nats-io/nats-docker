b486da9a110f9bffb40d23ec80238acc8b022fdd
Neil <neil@nats.io>
2025-09-09 16:29:30 +0100

    Release v2.11.9 (#261)
    Signed-off-by: Neil Twigg <neil@nats.io>

--- 2.11.x/tests/build-images.sh
+++ 2.11.x/tests/build-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-ver=(NATS_SERVER 2.11.9-RC.3)
+ver=(NATS_SERVER 2.11.9)
 
 (
 	cd "../alpine3.22"
--- 2.11.x/tests/run-images.sh
+++ 2.11.x/tests/run-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-ver=(NATS_SERVER 2.11.9-RC.3)
+ver=(NATS_SERVER 2.11.9)
 
 images=(
 	"nats:${ver[1]}-alpine3.22"
