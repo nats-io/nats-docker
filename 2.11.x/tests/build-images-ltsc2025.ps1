ea0e650d5fe6fe808e5b5cf5e65fb1abcacb3f4d
Neil Twigg <neil@nats.io>
2025-09-30 16:19:28 +0100

    Release v2.11.10
    Signed-off-by: Neil Twigg <neil@nats.io>

--- 2.11.x/tests/build-images-ltsc2025.ps1
+++ 2.11.x/tests/build-images-ltsc2025.ps1
@@ -3,7 +3,7 @@ Set-PSDebug -Trace 2
 # Exit on error.
 $ErrorActionPreference = "Stop"
 
-$ver = 'NATS_SERVER 2.11.10-RC.1'.Split(' ')[1]
+$ver = 'NATS_SERVER 2.11.10'.Split(' ')[1]
 
 Write-Output '-- host info ---'
 Write-Output $PSVersionTable
