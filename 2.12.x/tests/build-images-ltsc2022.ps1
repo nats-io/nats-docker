276c0494234840c63a52088c4edeade466dd0d7c
Neil Twigg <neil@nats.io>
2025-09-30 16:32:26 +0100

    Revert 2.12.1-RC.1 to 2.12.0 for docker-library/official-images#15524
    Signed-off-by: Neil Twigg <neil@nats.io>

--- 2.12.x/tests/build-images-ltsc2022.ps1
+++ 2.12.x/tests/build-images-ltsc2022.ps1
@@ -3,7 +3,7 @@ Set-PSDebug -Trace 2
 # Exit on error.
 $ErrorActionPreference = "Stop"
 
-$ver = 'NATS_SERVER 2.12.1-RC.1'.Split(' ')[1]
+$ver = 'NATS_SERVER 2.12.0'.Split(' ')[1]
 
 Write-Output '-- host info ---'
 Write-Output $PSVersionTable
