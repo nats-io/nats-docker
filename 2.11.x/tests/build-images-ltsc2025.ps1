87b4213a371331ff711dc9dce9a624d4b08e1871
Neil <neil@nats.io>
2026-01-15 17:30:34 +0000

    Releases v2.12.4-RC.2, v2.11.12-RC.3 (#293)
    Signed-off-by: Neil Twigg <neil@nats.io>
    
    ---------
    
    Signed-off-by: Neil Twigg <neil@nats.io>

--- 2.11.x/tests/build-images-ltsc2025.ps1
+++ 2.11.x/tests/build-images-ltsc2025.ps1
@@ -3,7 +3,7 @@ Set-PSDebug -Trace 2
 # Exit on error.
 $ErrorActionPreference = "Stop"
 
-$ver = 'NATS_SERVER 2.11.12-RC.2'.Split(' ')[1]
+$ver = 'NATS_SERVER 2.11.12-RC.3'.Split(' ')[1]
 
 Write-Output '-- host info ---'
 Write-Output $PSVersionTable
