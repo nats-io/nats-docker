3c05a33ef8ba8e27b0504f78dbc1804a6ba54a7f
Neil <neil@nats.io>
2026-05-29 16:41:19 +0100

    Releases v2.14.2-RC.1, v2.12.10-RC.1 (#311)
    Signed-off-by: Neil Twigg <neil@nats.io>
    
    ---------
    
    Signed-off-by: Neil Twigg <neil@nats.io>

--- 2.12.x/tests/build-images.sh
+++ 2.12.x/tests/build-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-ver=(NATS_SERVER 2.12.9)
+ver=(NATS_SERVER 2.12.10-RC.1)
 
 (
 	cd "../alpine3.22"
--- 2.14.x/tests/build-images.sh
+++ 2.14.x/tests/build-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-ver=(NATS_SERVER 2.14.1)
+ver=(NATS_SERVER 2.14.2-RC.1)
 
 (
 	cd "../alpine3.22"
