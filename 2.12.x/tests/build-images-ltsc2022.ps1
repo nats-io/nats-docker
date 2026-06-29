2382d403fed90db56bd9e4be237ebae56c55c8ff
Neil <neil@nats.io>
2026-06-29 16:43:53 +0100

    Releases v2.14.3, v2.12.12 (#314)
    Signed-off-by: Neil Twigg <neil@nats.io>
    
    ---------
    
    Signed-off-by: Neil Twigg <neil@nats.io>

--- 2.12.x/tests/build-images-ltsc2022.ps1
+++ 2.12.x/tests/build-images-ltsc2022.ps1
@@ -3,7 +3,7 @@ Set-PSDebug -Trace 2
 # Exit on error.
 $ErrorActionPreference = "Stop"
 
-$ver = 'NATS_SERVER 2.12.11'.Split(' ')[1]
+$ver = 'NATS_SERVER 2.12.12'.Split(' ')[1]
 
 Write-Output '-- host info ---'
 Write-Output $PSVersionTable
--- 2.12.x/tests/run-images.sh
+++ 2.12.x/tests/run-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-ver=(NATS_SERVER 2.12.11)
+ver=(NATS_SERVER 2.12.12)
 
 images=(
 	"nats:${ver[1]}-alpine3.22"
